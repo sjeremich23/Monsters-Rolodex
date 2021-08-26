fd5013f2bbbb50595f93ca6b38f26d94dfa706f1
Shane Jeremich <shane.jeremich@gmail.com>
2021-08-26 00:48:49 -0600

    Updates

---
+++ precache-manifest.5543a9c31fb6069b9831af1ff6a80887.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b4cc63cdfb1e74b8b0f67d80a98b7c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c1c53efd526470535265",
+    "url": "/monsters-rolodex/static/css/main.08f4eb75.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "c1c53efd526470535265",
+    "url": "/monsters-rolodex/static/js/main.91cb8cc4.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
